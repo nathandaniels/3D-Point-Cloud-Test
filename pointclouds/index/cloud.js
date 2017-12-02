79e67ca7d6f676ad9e849ea1dda11e8c4032e392
Nathan Daniels <nathandaniels@Nathans-Air.gateway>
2017-12-02 15:57:09 +1030

    Initial Commit

---
+++ pointclouds/index/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 1781014,
+    "boundingBox": {
+        "lx": 436848.006,
+        "ly": 4572668.637,
+        "lz": 220.632,
+        "ux": 437196.769,
+        "uy": 4573017.4,
+        "uz": 569.3949999999772
+    },
+    "tightBoundingBox": {
+        "lx": 436848.01,
+        "ly": 4572668.64,
+        "lz": 220.63,
+        "ux": 437196.77,
+        "uy": 4572940.62,
+        "uz": 267.07
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 3.020376205444336,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
